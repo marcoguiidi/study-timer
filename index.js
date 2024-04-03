3a196979bdd75d8e458593ecb895594617c0873a
marco <marco.guidi24@studio.unibo.it>
2024-04-03 11:31:05 +0200

    audio

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 // Aggiunge un listener per l'evento di submit al form con id 'studyForm'
 document.getElementById('studyForm').addEventListener('submit', function(event) {
     // Previene il comportamento di default dell'evento, che sarebbe il submit del form
@@ -7,9 +9,6 @@ document.getElementById('studyForm').addEventListener('submit', function(event) 
     const studyTime = parseInt(document.getElementById('studyTime').value, 10);
     // Converte i minuti in secondi per l'animazione
     const animationDuration = studyTime * 60; 
-
-
-    const audio = new Audio('timer_sound.mp3');
   
     // Imposta l'animazione con durata dinamica per gli pseudo-elementi ::before e ::after
     const styleSheet = document.createElement("style");
@@ -34,8 +33,6 @@ document.getElementById('studyForm').addEventListener('submit', function(event) 
     `;
     // Aggiunge il foglio di stile creato all'elemento head del documento
     document.head.appendChild(styleSheet);
-
-    
   
     // Calcola il tempo di fine aggiungendo la durata del timer al tempo corrente
     const endTime = Date.now() + studyTime * 60000; 
@@ -44,47 +41,51 @@ document.getElementById('studyForm').addEventListener('submit', function(event) 
     // Imposta un intervallo che si ripete ogni secondo
     const interval = setInterval(function() {
       
-    const now = Date.now();
-    // Calcola la differenza tra il tempo di fine e il tempo corrente
-    var difference = endTime - now;
+      const now = Date.now();
+      // Calcola la differenza tra il tempo di fine e il tempo corrente
+      var difference = endTime - now;
 
-    // Se la differenza è minore o uguale a 0, ferma l'intervallo
-    if (difference <= 0) {
-      clearInterval(interval);
+      // Se la differenza è minore o uguale a 0, ferma l'intervallo
+      if (difference <= 0) {
+        clearInterval(interval);
+        
+        let audio = document.getElementById("endAudio");
+        audio.play();
+        
+        setTimeout(function(){
+          audio.pause();
+
+          const styleSheet = document.createElement("style");
+          styleSheet.innerText = `
+          .hider{
+            display: flex;
+          }
+
+          .timer{
+            display: none;
+          }
+
+          #stop{
+            visibility: hidden;
+          }
+          `;
+          // Aggiunge il foglio di stile creato all'elemento head del documento
+          document.head.appendChild(styleSheet);
+          
+          // Pulisce il testo dell'elemento con id 'timerDisplay'
+          document.getElementById('timerDisplay').textContent = ""; 
+        }, 3000);
+        return;
+      }
+
+      // Calcola i minuti e i secondi rimanenti
+      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
       
-      // audio.play();
-      // const delay = (n) => new Promise(r => setTimeout(r, n * 1000));
-      // delay(2);
-      // audio.pause();
-
-      const styleSheet = document.createElement("style");
-      styleSheet.innerText = `
-      .hider{
-        display: flex;
-      }
-
-      .timer{
-        display: none;
-      }
-
-      #stop{
-        visibility: hidden;
-      }
-      `;
-      // Aggiunge il foglio di stile creato all'elemento head del documento
-      document.head.appendChild(styleSheet);
-      
-      // Pulisce il testo dell'elemento con id 'timerDisplay'
-      document.getElementById('timerDisplay').textContent = ""; 
-      return;
-    }
-
-    // Calcola i minuti e i secondi rimanenti
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-    
-    // Visualizza il tempo rimanente nell'elemento con id 'timerDisplay'
-    //padstart aggiunge uno zero prima della stringa se non raggiunge almeno una lunghezza di 2
+      // Visualizza il tempo rimanente nell'elemento con id 'timerDisplay'
+      //padstart aggiunge uno zero prima della stringa se non raggiunge almeno una lunghezza di 2
       document.getElementById('timerDisplay').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
     }, 1000);
   });
+
+  
