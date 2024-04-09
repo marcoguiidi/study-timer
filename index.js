c50df50a0a1a1ada1a7717bac27cc33a6257d0db
marco <marco.guidi24@studio.unibo.it>
2024-04-09 12:59:57 +0200

    final edit

--- index.js
+++ index.js
@@ -1,184 +1,155 @@
 let bgMusic = false;
 
-// Aggiunge un listener per l'evento di submit al form con id 'studyForm'
 document.getElementById('studyForm').addEventListener('submit', function(event) {
-    // Previene il comportamento di default dell'evento, che sarebbe il submit del form
-    event.preventDefault();
+  event.preventDefault();
 
-    let studing = true;
+  let music = document.getElementById("inAudio");
 
-    let music = document.getElementById("inAudio");
-    if(bgMusic){
+  let contatore = 0;
+  const studyTime = parseInt(document.getElementById('studyTime').value, 10);
+  const pauseTime = parseInt(document.getElementById('pauseTime').value, 10);
+  const times = parseInt(document.getElementById('times').value, 10);
+  const animationDuration = studyTime * 60;
+
+  function avviaTimer() {
+    contatore++;
+    if (bgMusic) {
       music.play();
     }
-    // Ottiene il tempo di studio inserito dall'utente e lo converte in un numero intero
-    const studyTime = parseInt(document.getElementById('studyTime').value, 10);
-    const pauseTime = parseInt(document.getElementById('pauseTime').value, 10);
-    const times = parseInt(document.getElementById('times').value, 10);
-    // Converte i minuti in secondi per l'animazione
-    const animationDuration = studyTime * 60; 
   
-    // Imposta l'animazione con durata dinamica per gli pseudo-elementi ::before e ::after
-    const styleSheet = document.createElement("style");
-    styleSheet.innerText = `
-      body {
-        animation: bg ${animationDuration}s 1;
-      }
-
-      .hider{
-        display: none;
-      }
-
-      .timer{
-        display:flex;
-        animation: timer 5s 1;
-      }
-
-      #stop{
-        visibility: visible;
-        animation: sButton ${animationDuration}s 1;
-      }
-    `;
-    // Aggiunge il foglio di stile creato all'elemento head del documento
-    document.head.appendChild(styleSheet);
-  
-    // Calcola il tempo di fine aggiungendo la durata del timer al tempo corrente
-     
-  
+    const styleSheet = createStyleSheet(`
+    body {
+      animation: bg ${animationDuration}s 1;
+    }
     
-    for (let i = 0; i < times; i++) {
-      if(studing){
-        // Imposta un intervallo che si ripete ogni secondo
-        let endStudyTime = Date.now() + studyTime * 60000;
-        const interval = setInterval(function() {
-          
-          const now = Date.now();
-          // Calcola la differenza tra il tempo di fine e il tempo corrente
-          var difference = endStudyTime - now;
+    .hider {
+      display: none;
+    }
+    
+    .timer {
+      display: flex;
+      animation: timer 5s 1;
+    }
+  
+    #pause {
+      display: none;
+    }
+    
+    #stop {
+      visibility: visible;
+      animation: sButton ${animationDuration}s 1;
+    }
+    `);
 
-          // Se la differenza è minore o uguale a 0, ferma l'intervallo
-          if (difference <= 0) {
-            clearInterval(interval);
-            music.pause()
-            
-            let audio = document.getElementById("endAudio");
-            audio.play();
-            
-            setTimeout(function(){
-              audio.pause();
+    document.getElementById("task").textContent = "STUDY TIME";
+  
+    const endStudyTime = Date.now() + studyTime * 60000;
+    const studyInterval = startInterval(endStudyTime, function() {
+      clearInterval(studyInterval);
+      music.pause();
+      let audio = document.getElementById("endAudio");
+      audio.play();
+      setTimeout(function() {
+        audio.pause();
+        document.getElementById('timerDisplay').textContent = "00:00";
+        removeStyleSheet(styleSheet);
+        avviaPausa();
+      }, 3000);
+    }, function(difference) {
+      const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+      document.getElementById('timerDisplay').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    });
+  }
+  
+  function avviaPausa() {
+    const styleSheet = createStyleSheet(`
+    #stop {
+      visibility: visible;
+    }
+  
+    .hider {
+      display: none;
+    }
+  
+    #pause {
+      display: flex;
+    }
+  
+    #clock {
+      display: none;
+    }
+    `);
 
-              const styleSheet = document.createElement("style");
-              styleSheet.innerText = `
-              // .hider{
-              //   display: flex;
-              // }
+    document.getElementById("task").textContent = "PAUSE TIME";
 
-              // timer{
-              //   display: none;
-              // }
+  
+    const endPauseTime = Date.now() + pauseTime * 60000;
+    const pauseInterval = startInterval(endPauseTime, function() {
+      clearInterval(pauseInterval);
+      document.getElementById('pauseTimer').textContent = "00:00";
+      
+      document.getElementById("task").textContent = "";
+      
+      let trumpet = document.getElementById("trumpets");
+      trumpet.play();
+      setTimeout(function(){
+        trumpet.pause();
+        removeStyleSheet(styleSheet);
+        if (contatore < times) {
+          avviaTimer();
+        }
+      }, 3000);
+      
+    },function(difference) {
+        const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+        document.getElementById('pauseTimer').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+      });
+  }
 
-              #stop{
-                visibility: hidden;
-              }
+  if (contatore < times) {
+    avviaTimer();
+  }
+});
 
-              #clock{
-                display: none;
-              }
+function createStyleSheet(cssText) {
+  const styleSheet = document.createElement("style");
+  styleSheet.innerText = cssText;
+  document.head.appendChild(styleSheet);
+  return styleSheet;
+}
 
-              #pause{
-                display: flex;
-              }
-              `;
-              document.head.appendChild(styleSheet);
-              
-              studing = false;
-            }, 3000);
-          }
+function removeStyleSheet(styleSheet) {
+  styleSheet.parentNode.removeChild(styleSheet);
+}
 
-          // Calcola i minuti e i secondi rimanenti
-          const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-          const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-          
-          // Visualizza il tempo rimanente nell'elemento con id 'timerDisplay'
-          //padstart aggiunge uno zero prima della stringa se non raggiunge almeno una lunghezza di 2
-          document.getElementById('timerDisplay').textContent = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-        }, 1000);
-      }
+function startInterval(endTime, callback, updateClockCallback) {
+  return setInterval(function() {
+    const now = Date.now();
+    let difference = endTime - now;
 
-      if(!studing){
-        let endPauseTime = Date.now() + pauseTime * 60000;
-        music.play();
-        const pause = setInterval(function() {
-          
-          const now = Date.now();
-          // Calcola la differenza tra il tempo di fine e il tempo corrente          
-          var Pdifference = endPauseTime - now;
-
-          // Se la differenza è minore o uguale a 0, ferma l'intervallo
-          if (Pdifference <= 0) {
-            clearInterval(pause);
-            music.pause();
-            
-            const styleSheet = document.createElement("style");
-            styleSheet.innerText = `
-            .hider{
-              display: flex;
-            }
-
-            .timer{
-              display: none;
-            }
-
-            #pause{
-              display: none;
-            }
-
-            #stop{
-              visibility: hidden;
-            }
-            `;
-            document.head.appendChild(styleSheet);
-            
-            // Pulisce il testo dell'elemento con id 'timerDisplay'
-            document.getElementById('pauseTimer').textContent = ""; 
-          
-            studing = true;
-          }
-
-          // Calcola i minuti e i secondi rimanenti
-          const Pminutes = Math.floor((Pdifference % (1000 * 60 * 60)) / (1000 * 60));
-          const Pseconds = Math.floor((Pdifference % (1000 * 60)) / 1000);
-          
-
-          document.getElementById('pauseTimer').textContent = `${String(Pminutes).padStart(2, '0')}:${String(Pseconds).padStart(2, '0')}`;
-        }, 1000);
+    if (difference <= 0) {
+      callback();
+    } else {
+      if (updateClockCallback) {
+        updateClockCallback(difference);
       }
     }
-    return;
-});
+  }, 1000);
+}
+
 
 document.getElementById("music").addEventListener("click", function(event){
   event.preventDefault();
 
-  //evidenzia il pulsante in maniera differente in base alla selezione
-  if (bgMusic){  
-    bgMusic = false;
-    const styleSheet = document.createElement("style");
-    styleSheet.innerText = `
-    #music{
-      opacity: 0.6;
+  // Invertiamo lo stato della musica di background e cambiamo lo stile del pulsante
+  bgMusic = !bgMusic;
+  const opacity = bgMusic ? 1 : 0.6;
+  const styleSheet = createStyleSheet(`
+    #music {
+      opacity: ${opacity};
     }
-    `;
-    document.head.appendChild(styleSheet);
-  } else {
-    bgMusic = true;
-    const styleSheet = document.createElement("style");
-    styleSheet.innerText = `
-    #music{
-      opacity: 1;
-    }
-    `;
-    document.head.appendChild(styleSheet);
-  }
+  `);
 });
-  
+
